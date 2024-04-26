195ccf438d87deaa1dce4273b0941ba0a6111aa8
Ben Jarrett <b.cl.jarrett@gmail.com>
2024-04-26 16:22:56 -0400

    Initial Commit

---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,17 @@
+using System;
+using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformsProfile : Profile
+    {
+        public PlatformsProfile()
+        {
+            // Source -> Target //
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+    }
+}
