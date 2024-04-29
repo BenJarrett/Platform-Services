c9ebbe68ccfed09f4e1b6589829fcfce392b56ec
Ben Jarrett <b.cl.jarrett@gmail.com>
2024-04-29 10:54:58 -0400

    Created a nodeport for kubernetes. Setup postman up for port testing

--- PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a0f1586e98ac35af545b923c7d151c5a8e7339")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
