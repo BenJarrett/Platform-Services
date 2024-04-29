b0c87caab539477f3210fd1f62a66a27c52e6538
Ben Jarrett <b.cl.jarrett@gmail.com>
2024-04-29 14:21:14 -0400

    fixed controller routing name for second service

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -2,7 +2,9 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
-{
+{   
+    [Route("api/c/[controller]")]
+    [ApiController]
     public class PlatformsController : ControllerBase
     {
         
