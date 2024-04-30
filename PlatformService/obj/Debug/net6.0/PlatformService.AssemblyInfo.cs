712d1e82a141d64d048947dba0f63571b8c18cb9
Ben Jarrett <b.cl.jarrett@gmail.com>
2024-04-30 17:02:11 -0400

    Continued

--- PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net6.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c86ff6951f59948f595e34d60d24a16007f9103e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523829bb4644f377a51f06952f814e0ffd640618")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
